d84ce4d930ebd1214f0410aee99c4a868931cd04
Guilherme Lima <guilimadev@gmail.com>
2022-11-19 16:51:33 -0300

    First Commit

--- main.py
+++ main.py
@@ -18,7 +18,14 @@ df_all_players.index = [''] * len(df_all_players)
 df_all_players = df_all_players.fillna(0)
 
 
-
+df_per48 = df_all_players
+df_per48['APG'] = df_per48['APG'] * 48 / df_per48['MPG']
+df_per48['PPG'] = df_per48['PPG'] * 48 / df_per48['MPG']
+df_per48['TPG'] = df_per48['TPG'] * 48 / df_per48['MPG']
+df_per48['BPG'] = df_per48['BPG'] * 48 / df_per48['MPG']
+df_per48['SPG'] = df_per48['SPG'] * 48 / df_per48['MPG']
+df_per48['RPG'] = df_per48['RPG'] * 48 / df_per48['MPG']
+df_per48 = df_per48.fillna(0)
 
 with all_players:
     col1, col2 = st.columns(2)
@@ -31,34 +38,28 @@ with all_players:
 
 with per_team:
     selecionar_time = st.selectbox(options=df_all_players.Team.unique(), label="Selecione seu time: ")
-    if selecionar_time:      
-        st.dataframe(df_all_players[(df_all_players['Team'] == selecionar_time)].style.format(subset=['MPG','APG', 'SPG', 'RPG', 'BPG', 'TPG', 'PPG'], formatter="{:.1f}")) 
+    col1, col2 = st.columns(2)    
+    if selecionar_time:
+        with col1:
+            st.header("Stats:")      
+            st.dataframe(df_all_players[(df_all_players['Team'] == selecionar_time)].style.format(subset=['MPG','APG', 'SPG', 'RPG', 'BPG', 'TPG', 'PPG'], formatter="{:.1f}")) 
+        with col2:
+            st.header("Stats por 48 minutos:")
+            st.dataframe(df_per48[(df_per48['Team'] == selecionar_time)].style.format(subset=['MPG','APG', 'SPG', 'RPG', 'BPG', 'TPG', 'PPG'], formatter="{:.1f}")) 
 
-with per_48:
-    df_per48 = df_all_players
-    df_per48['APG'] = df_per48['APG'] * 48 / df_per48['MPG']
-    df_per48['PPG'] = df_per48['PPG'] * 48 / df_per48['MPG']
-    df_per48['TPG'] = df_per48['TPG'] * 48 / df_per48['MPG']
-    df_per48['BPG'] = df_per48['BPG'] * 48 / df_per48['MPG']
-    df_per48['SPG'] = df_per48['SPG'] * 48 / df_per48['MPG']
-    df_per48['RPG'] = df_per48['RPG'] * 48 / df_per48['MPG']
-    df_per48 = df_per48.fillna(0)
+
+with per_48:   
     list_of_teams = np.empty(1)
     list_of_teams = np.append(list_of_teams,"Todos")
     list_of_teams = np.delete(list_of_teams, 0)
     list_of_teams = np.append(list_of_teams,df_all_players.Team.unique()) 
     
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     with col1:
         mpg_slider_48 = st.slider('MPG:', 0.0, 48.0, (0.0,48.0), key=2)        
     with col2:
-        selecionar_time_48 = st.selectbox(options=list_of_teams, label="Selecione seu time: ", key=4)
-    with col3:
         games_slider_48 = st.slider('Min Games:', 0, 82, 0, key=3)
 
-    if selecionar_time_48 == "Todos":
-        st.dataframe(df_per48[(df_per48['MPG'] <= mpg_slider_48[1]) & (df_per48['MPG'] >= mpg_slider_48[0]) & (df_per48['Games'] >= games_slider_48)].style.format(subset=['MPG','APG', 'SPG', 'RPG', 'BPG', 'TPG', 'PPG'], formatter="{:.1f}"))
-    else:
-        st.dataframe(df_per48[(df_per48['MPG'] <= mpg_slider_48) & (df_per48['Games'] >= games_slider_48) & (df_per48['Team'] == selecionar_time_48)].style.format(subset=['MPG','APG', 'SPG', 'RPG', 'BPG', 'TPG', 'PPG'], formatter="{:.1f}"))    
-
+    
+    st.dataframe(df_per48[(df_per48['MPG'] <= mpg_slider_48[1]) & (df_per48['MPG'] >= mpg_slider_48[0]) & (df_per48['Games'] >= games_slider_48)].style.format(subset=['MPG','APG', 'SPG', 'RPG', 'BPG', 'TPG', 'PPG'], formatter="{:.1f}"))
    
