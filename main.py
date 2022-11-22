e690cc638c8f80f9e302595cf96dadca1e5eb23c
Guilherme Lima <guilimadev@gmail.com>
2022-11-21 22:59:12 -0300

    cleaning files

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ st.set_page_config(page_title='STB Stats', page_icon=':basketball:', layout='wid
 
 
 db = firestore.Client.from_service_account_json("firestore-key.json")
-#teams = list(db.collection(u'teams').stream())
+import json
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="streamlit-reddit")
 
 teams = db.collection("teams")
 df = pd.DataFrame()
