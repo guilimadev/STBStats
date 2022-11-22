aaf22aa6a0606b78c947cda4d72832c9d0bfc91d
Guilherme Lima <guilimadev@gmail.com>
2022-11-22 20:58:24 -0300

    Split df build file

---
+++ dfs_builder.py
@@ -0,0 +1,35 @@
+from google.cloud import firestore
+from google.oauth2 import service_account
+import pandas as pd
+import streamlit as st
+import numpy as np
+import json
+
+
+
+
+#db = firestore.Client.from_service_account_json("firestore-key.json")
+
+
+@st.cache(suppress_st_warning=True)
+def create_df():
+    key_dict = json.loads(st.secrets["textkey"])
+    creds = service_account.Credentials.from_service_account_info(key_dict)
+    db = firestore.Client(credentials=creds)
+    teams = db.collection("teams")
+    df = pd.DataFrame()
+    dfs = []
+    for team in teams.stream():
+        df_team = pd.DataFrame()
+        doc2 = team.to_dict()   
+        team_name = doc2['name']    
+        players = db.collection('teams').document(f"{team_name}").collection('players').stream()
+        
+
+        players_dict = list(map(lambda x: x.to_dict(), players))
+        df_team = pd.DataFrame.from_dict(players_dict)
+        df_team = df_team.assign(Team=team_name)
+        dfs.append(df_team)
+
+    print('Enter')
+    return dfs
--- main.py
+++ main.py
@@ -3,32 +3,13 @@ from google.oauth2 import service_account
 import pandas as pd
 import streamlit as st
 import numpy as np
+from dfs_builder import create_df
 import json
 
 st.set_page_config(page_title='STB Stats', page_icon=':basketball:', layout='wide')
 
 
-
-#db = firestore.Client.from_service_account_json("firestore-key.json")
-key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds)
-
-teams = db.collection("teams")
-df = pd.DataFrame()
-dfs = []
-for team in teams.stream():
-    df_team = pd.DataFrame()
-    doc2 = team.to_dict()   
-    team_name = doc2['name']    
-    players = db.collection('teams').document(f"{team_name}").collection('players').stream()
-    
-
-    players_dict = list(map(lambda x: x.to_dict(), players))
-    df_team = pd.DataFrame.from_dict(players_dict)
-    df_team = df_team.assign(Team=team_name)
-    dfs.append(df_team)
-
+dfs = create_df()
 
 
 st.title("STB Stats by " + ':bear:')
